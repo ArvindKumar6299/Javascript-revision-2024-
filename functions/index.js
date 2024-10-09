44048d938d824dc8e0acf334d93354ae4bbe7989
Arvind Kumar <arvindkumar735240@gmail.com>
2024-10-10 01:25:39 +0530

    loops

---
+++ functions/index.js
@@ -0,0 +1,17 @@
+// Functions =>  a block of code that performs a particular task
+
+
+function arvind(){
+    const value = 97*76;
+    console.log("result is: ",value);
+}
+
+arvind();
+
+
+function  Data(a,b){
+    const result =  a*b+b;
+    console.log("a*b+b = ",result);
+}
+
+Data(53,21);
